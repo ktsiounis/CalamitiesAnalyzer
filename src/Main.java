aa817e6fc3d15dece3900a5ada4f5b8da7ff6226
Alexios Polyzos <dragotic.saen@gmail.com>
2019-03-31 17:17:58 +0300

    Copyrights

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+/*
+ * ChainProof
+ * Tsiounis Konstantinos 2630
+ * Polyzos Alexios 2338
+ * Copyright (c) 2019.
+ */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("views/main_view.fxml"));
-        primaryStage.setTitle("Advanced Topics of Database Technology and Application");
+        primaryStage.setTitle("ChainProof Graph Builder");
         primaryStage.setScene(new Scene(root, 860, 805));
         primaryStage.show();
     }
--- src/models/Country.java
+++ src/models/Country.java
@@ -1,3 +1,10 @@
+/*
+ * ChainProof
+ * Tsiounis Konstantinos 2630
+ * Polyzos Alexios 2338
+ * Copyright (c) 2019.
+ */
+
 package models;
 
 public class Country {
